2afb04f0e38a1bd38662c98349af07f4d4091017
Michael Caley <kneedeepwater@hotmail.com>
2021-10-21 21:18:52 +0100

    test send email

--- README.md
+++ README.md
@@ -6,5 +6,16 @@ Function is deployed on merge to master via github actions.
 
 Documentation for AWS cli docs (used to deploy) can be found [Here](https://awscli.amazonaws.com/v2/documentation/api/latest/reference/index.html#cli-aws)
 
+## Example query
+
+'{
+  "name": "Michael",
+  "email": "Kneedeepwater@hotmail.com",
+  "telephone": "07757770889",
+  "inquiry": "Install",
+  "emailDest": "default"
+}
+'
+
 ### Notes on lambda
 Lambda functions require the async keyword in order to return a value. It's not too clear from AWS docs but it appears that the only way to return a value outside of using async keyword is to use the callback parameter. [Docs](https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
--- index.js
+++ index.js
@@ -4,33 +4,28 @@ const emailDestOptions = { default: "kneedeepwater@hotmail.com" };
 
 exports.handler = async (event, context) => {
   const { name, email, telephone, inquiry, emailDest } = event;
-  if ((!name, !email, !telephone, !inquiry, !emailDest)) {
+  if ((!name, !email, !telephone, !inquiry, !emailDestOptions[emailDest])) {
     return "Incomplete details";
   }
 
-  return "Complete details";
+  let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    secure: true,
+    port: 465,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_KEY,
+    },
+  });
 
-  // let transporter = nodemailer.createTransport({
-  //   host: process.env.EMAIL_HOST,
-  //   secure: true,
-  //   port: 465,
-  //   auth: {
-  //     user: process.env.EMAIL_USER,
-  //     pass: process.env.EMAIL_KEY,
-  //   },
-  // });
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: emailDest,
+    subject: "webComments",
+    html: `${name} ${email} ${telephone} ${inquiry}`,
+  };
 
-  // const mailOptions = {
-  //   from: process.env.EMAIL_USER,
-  //   to: emailDest,
-  //   subject: "webComments",
-  //   html: `${name} ${email} ${telephone} ${inquiry}`,
-  // };
+  const response = await transporter.sendMail(mailOptions);
 
-  // try {
-  //   const response = await transporter.sendMail(mailOptions);
-  //   res.send({ response });
-  // } catch (e) {
-  //   res.send({ e });
-  // }
+  return response;
 };
