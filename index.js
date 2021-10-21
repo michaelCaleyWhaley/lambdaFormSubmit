2660f9fc7d9741b07b750f978886d4dbd0490996
Michael Caley <kneedeepwater@hotmail.com>
2021-10-21 20:49:23 +0100

    remove async

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 exports.handler = (event, context) => {
-    return 'Hello world';
+  console.log("value1 =", event.key1);
+  console.log("value2 =", event.key2);
+  console.log("value3 =", event.key3);
+  return event.key1;
 };
