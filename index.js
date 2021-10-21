6ef6000b847942836e14e4fd714aed8ac0fc0bcb
Michael Caley <kneedeepwater@hotmail.com>
2021-10-21 20:44:35 +0100

    refine export

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-exports.handler = function (context) {
-  console.log(context);
-  return "Working";
+console.log('Loading function');
+
+exports.handler = async (event, context) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('value1 =', event.key1);
+    console.log('value2 =', event.key2);
+    console.log('value3 =', event.key3);
+    return event.key1;  // Echo back the first key value
+    // throw new Error('Something went wrong');
 };
