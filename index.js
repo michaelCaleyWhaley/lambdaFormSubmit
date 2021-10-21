442d8f5238017f26006728c9e565add15a3a5b65
Michael Caley <kneedeepwater@hotmail.com>
2021-10-21 20:51:50 +0100

    test string

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ exports.handler = async (event, context) => {
   console.log("value1 =", event.key1);
   console.log("value2 =", event.key2);
   console.log("value3 =", event.key3);
-  return event.key1;
+  return 'hello world';
 };
