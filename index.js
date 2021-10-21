5c5bbe181f48423aa31dec35d4a27cda18bf66d9
Michael Caley <kneedeepwater@hotmail.com>
2021-10-21 20:46:43 +0100

    hello world

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-console.log('Loading function');
-
-exports.handler = async (event, context) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log('value1 =', event.key1);
-    console.log('value2 =', event.key2);
-    console.log('value3 =', event.key3);
-    return event.key1;  // Echo back the first key value
-    // throw new Error('Something went wrong');
+exports.handler = (event, context) => {
+    return 'Hello world';
 };
