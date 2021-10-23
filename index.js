0079af786a5ece22c03db7dafe40a59da17e6b56
Michael Caley <kneedeepwater@hotmail.com>
2021-10-23 12:33:27 +0100

    test lambda type module

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const nodemailer = require("nodemailer");
+import nodemailer from "nodemailer";
+// const nodemailer = require("nodemailer");
 
 const emailDestOptions = { default: "kneedeepwater@hotmail.com" };
 
 // eslint-disable-next-line no-unused-vars
-exports.handler = async (event, context) => {
-  const { name, email, telephone, inquiry, emailDest } = event;
-  if ((!name, !email, !telephone, !inquiry, !emailDestOptions[emailDest])) {
-    return "Incomplete details";
-  }
+const handler = async (event, context) => {
+  // console.log(`nodemailer: `, nodemailer);
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    secure: true,
-    port: 465,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_KEY,
-    },
-  });
+  return JSON.stringify({ test: "hello" });
 
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: emailDest,
-    subject: "webComments",
-    html: `${name} ${email} ${telephone} ${inquiry}`,
-  };
+  // const { name, email, telephone, inquiry, emailDest } = event;
+  // if ((!name, !email, !telephone, !inquiry, !emailDestOptions[emailDest])) {
+  //   return "Incomplete details";
+  // }
 
-  const response = await transporter.sendMail(mailOptions);
+  // const transporter = nodemailer.createTransport({
+  //   host: process.env.EMAIL_HOST,
+  //   secure: true,
+  //   port: 465,
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_KEY,
+  //   },
+  // });
 
-  return response;
+  // const mailOptions = {
+  //   from: process.env.EMAIL_USER,
+  //   to: emailDest,
+  //   subject: "webComments",
+  //   html: `${name} ${email} ${telephone} ${inquiry}`,
+  // };
+
+  // const response = await transporter.sendMail(mailOptions);
+
+  // return response;
 };
+
+console.log(`handler(): `, handler());
+
+export { handler };
